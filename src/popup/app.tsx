82e2f5562924b0b808ecc0f4ab045968ca5dd61e
again7536 <45936642+again7536@users.noreply.github.com>
2022-10-17 14:50:05 +0900

    feat: add request test

--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,5 +1,18 @@
+import { fetchCourses, fetchStudentId } from "@/api";
+import { useEffect } from "react";
+
 function App() {
-  return <div>hi</div>;
+  useEffect(() => {
+    fetchCourses().then((data) =>
+      fetchStudentId({ courseId: data[0].id, userId: data[0].enrollments[0].user_id })
+    );
+  }, []);
+
+  return (
+    <div>
+      <p>hihi</p>
+    </div>
+  );
 }
 
 export default App;
