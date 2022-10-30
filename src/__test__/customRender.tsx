aa599d61fb72f0ce23ba020828ce52c557554b13
again7536 <45936642+again7536@users.noreply.github.com>
2022-10-30 15:38:50 +0900

    feat: add test mocking and setup

---
+++ src/__test__/customRender.tsx
@@ -0,0 +1,60 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { render, RenderOptions, waitFor, screen } from "@testing-library/react";
+import { ReactElement } from "react";
+import { QueryClient, useIsRestoring } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import persister from "@/utils/persister";
+import { Provider } from "jotai";
+import { ThemeProvider } from "@mui/material/styles";
+import GlobalStyle from "@/styles/global";
+import { Global } from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/styles/theme";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24,
+      staleTime: 1000 * 60 * 60 * 24,
+    },
+  },
+});
+
+function Hidden({ children }: { children: React.ReactNode }) {
+  const isRestoring = useIsRestoring();
+
+  return (
+    <>
+      <span>{isRestoring ? "Restoring" : "Restored"}</span>
+      {children}
+    </>
+  );
+}
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{
+        persister,
+        maxAge: 1000 * 60 * 60 * 24,
+      }}
+    >
+      <Provider>
+        <Global styles={GlobalStyle} />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Hidden>{children}</Hidden>
+        </ThemeProvider>
+      </Provider>
+    </PersistQueryClientProvider>
+  );
+}
+
+const customRender = async (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) => {
+  const result = render(ui, { wrapper: Providers, ...options });
+  await waitFor(() => screen.findByText("Restored"));
+  return result;
+};
+
+export { customRender as render };
---
+++ src/__test__/mock/assignments.ts
@@ -0,0 +1,71 @@
+import { faker } from "@faker-js/faker";
+import { AssignmentInfos } from "@/types";
+
+interface MockedAssignmentsFactoryParams {
+  amount: {
+    pdf?: number;
+    video?: number;
+    zoom?: number;
+    everlec?: number;
+    mp4?: number;
+    quiz?: number;
+    assignment?: number;
+  };
+}
+
+const mockedAssignmentsFactory = ({ amount }: MockedAssignmentsFactoryParams): AssignmentInfos[] =>
+  Array.from({ length: Object.values(amount).reduce((acc, val) => acc + val, 0) }).map(
+    (_, idx): AssignmentInfos => {
+      const assignmentId = +faker.random.numeric(7);
+      const types = Object.entries(amount)
+        .map(([key, val]) => Array.from({ length: val }, () => key))
+        .flat() as string[];
+      const isWork = types[idx] === "assginment" || types[idx] === "quiz";
+
+      return {
+        id: assignmentId,
+        assignment_id: assignmentId,
+        course_id: +faker.random.numeric(7),
+        component_id: +faker.random.numeric(7),
+        attendance_status: "false",
+        completed: false,
+        created_at: faker.date.past().toISOString(),
+        due_at: faker.date.future().toISOString(),
+        late_at: faker.date.future().toISOString(),
+        unlock_at: faker.date.past().toISOString(),
+        lock_at: "",
+        description: "",
+        external_extra_vars: {
+          canvas_content_id: +faker.random.numeric(7),
+        },
+        grade: null,
+        grading_type: "",
+        has_error_external_url: false,
+        is_master_course_child_content: true,
+        muted: false,
+        omit_from_final_grade: false,
+        opened: true,
+        points_possible: 1,
+        position: 1,
+        score: null,
+        submission_types: [],
+        submitted: false,
+        title: faker.lorem.word(7),
+        use_attendance: true,
+        view_info: { view_url: "" },
+        type: !isWork ? "commons" : types[idx],
+        commons_content: isWork
+          ? {
+              content_id: faker.random.alphaNumeric(7),
+              content_type: types[idx],
+              duration: +faker.random.numeric(3),
+              progress_support: true,
+              thumbnail_url: "",
+              view_url: "",
+            }
+          : undefined,
+      };
+    }
+  );
+
+export { mockedAssignmentsFactory };
