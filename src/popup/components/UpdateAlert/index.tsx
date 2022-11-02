19325ab3d72dcfed31f9b6877530189f89afac2c
again7536 <45936642+again7536@users.noreply.github.com>
2022-11-02 16:30:25 +0900

    feat: add update alert component test

--- src/popup/components/UpdateAlert/index.tsx
+++ src/popup/components/UpdateAlert/index.tsx
@@ -2,10 +2,16 @@ import { Alert, AlertTitle } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useAtom } from "jotai";
 import { versionAtom } from "@/atoms";
+import * as S from "./styled";
+
+const RELEASES_NOTE = "https://github.com/again7536/icampus-manager/releases";
 
 function UpdateAlert() {
   const [updated, setUpdated] = useState<boolean>(false);
   const [version, setVersion] = useAtom(versionAtom);
+  const handleClick = () => {
+    chrome.tabs.create({ url: RELEASES_NOTE });
+  };
 
   useEffect(() => {
     if (version && chrome.runtime.getManifest().version !== version) {
@@ -16,14 +22,15 @@ function UpdateAlert() {
     }
   }, [version, setVersion]);
 
-  return (
-    updated && (
-      <Alert severity="info">
-        <AlertTitle>Info</AlertTitle>
-        새로운 버전으로 업데이트 되었습니다. — <strong>변경 사항 보기</strong>
-      </Alert>
-    )
-  );
+  return updated ? (
+    <Alert sx={{ marginBottom: "5px" }} severity="info">
+      <AlertTitle>Info</AlertTitle>
+      새로운 버전으로 업데이트 되었습니다. —
+      <S.ReleaseLink role="link" onClick={handleClick}>
+        변경 사항 보기
+      </S.ReleaseLink>
+    </Alert>
+  ) : null;
 }
 
 export default UpdateAlert;
