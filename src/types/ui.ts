be3aa026f3af297021ea8be42fae65be2a2e01ed
again7536 <45936642+again7536@users.noreply.github.com>
2022-11-02 15:41:51 +0900

    feat: add settings and snackbar atoms

--- src/types/ui.ts
+++ src/types/ui.ts
@@ -8,4 +8,19 @@ interface Route {
   };
 }
 
-export type { Route };
+type SettingsKey = "DDAY";
+type SettingsInput = "switch" | "number";
+
+type Settings = {
+  [key in SettingsKey]: boolean | string;
+};
+
+type SettingsInfo = {
+  [key in SettingsKey]: {
+    title: string;
+    defaultValue: boolean | string;
+    type: SettingsInput;
+  };
+};
+
+export type { Route, SettingsKey, SettingsInput, Settings, SettingsInfo };
