7c6390f2aa2c57ad153a2f27c2ae659aa93e17a1
again7536 <45936642+again7536@users.noreply.github.com>
2022-11-07 14:06:47 +0900

    fix: material hook not to show invalidated pdf

--- src/hooks/useMemoMaterials.ts
+++ src/hooks/useMemoMaterials.ts
@@ -11,7 +11,12 @@ function useMemoMaterials({ results }: UseMemoMaterialsParams) {
     () =>
       results.map(
         (result) =>
-          result.data?.filter((datum) => datum.commons_content?.content_type === "pdf") ?? []
+          result.data?.filter(
+            (datum) =>
+              datum.commons_content?.content_type === "pdf" &&
+              // 학습 기간이 만료된 경우 (content_id === "not_open") 노출시키지 않음
+              datum.commons_content?.content_id !== "not_open"
+          ) ?? []
       ),
     [results]
   );
