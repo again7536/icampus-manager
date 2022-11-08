4c79ad898f54af181c7869016bbceab051493301
again7536 <45936642+again7536@users.noreply.github.com>
2022-11-08 11:29:08 +0900

    fix: change git case sensitive

---
+++ src/popup/sections/Settings/Settings.style.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const LinkerLine = styled.div`
+  height: 0px;
+  flex: 1.5 1 auto;
+  border-top: 2px dotted #3c3c3c80;
+`;
+
+const ButtonGroup = styled.div`
+  padding: 5px 15px;
+  margin-bottom: 10px;
+`;
+
+export { LinkerLine, ButtonGroup };
