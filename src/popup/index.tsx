aab73528c46f3ec1574edbd0ab1ebedb0db533cb
again7536 <45936642+again7536@users.noreply.github.com>
2022-11-19 15:15:18 +0900

    chore: version update

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production")
   Sentry.init({
     dsn: "https://8dd7e97aae254e40aefb19f1d554eeac@o4504121681641472.ingest.sentry.io/4504121692651520",
     integrations: [new BrowserTracing()],
-    release: "icampus-manager@1.1.1",
+    release: "icampus-manager@1.1.1.1",
     tracesSampleRate: 1.0,
   });
 
